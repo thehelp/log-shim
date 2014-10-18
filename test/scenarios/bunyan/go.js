851963d039ceb2491a4d87f2b8be8cd73218c74d
Scott Nonnenberg <scott@nonnenberg.com>
2014-10-18 15:35:23 -0700

    Update bunyan integration test: show monkey-patch

--- test/scenarios/bunyan/go.js
+++ test/scenarios/bunyan/go.js
@@ -7,6 +7,7 @@ var logShim = require('thehelp-log-shim');
 
 var logger = logShim('test:scenarios:bunyan');
 
+// this should not show up; the default level is info
 logger.verbose('this is the default verbose text');
 
 logger.info('this is the info text', {
@@ -23,17 +24,14 @@ logger.warn('this is the warn text', function(arg1, arg2, arg3, arg4) {
 logger.error('this is the error %s', 'interpolation');
 
 
-
-logShim.loadLogger = function(moduleName) {
-  var logger = bunyan.createLogger({
-    name: moduleName,
-    level: 'debug'
-  });
-
-  logger.verbose = logger.debug;
-  return logger;
+// monkey-patching to approximate global configuration
+var create = bunyan.createLogger;
+bunyan.createLogger = function(options) {
+  options.level = 'debug';
+  return create(options);
 };
 
+// this will show up
 var second = logShim('test:scenarios:bunyan:custom');
 
 second.verbose('this is the custom verbose text');
