5ebb2a9c6b5855ff90f62074502f9a2cc4036296
Scott Nonnenberg <scott@nonnenberg.com>
2014-10-18 10:19:45 -0700

    Add integration test for multi-install/version

---
+++ test/integration/server/test_multi.js
@@ -0,0 +1,65 @@
+
+'use strict';
+
+var path = require('path');
+
+var test = require('thehelp-test');
+var expect = test.expect;
+
+var logShim = require('../../../src/server');
+var pkg = require('../../../package.json');
+var origVersion = pkg.version;
+
+describe('multi', function() {
+
+  var deleteShimCache = function() {
+    var shim = path.join(__dirname, '../../../src/server/log_shim.js');
+    var index = path.join(__dirname, '../../../src/server/index.js');
+
+    delete require.cache[shim];
+    delete require.cache[index];
+  };
+
+  var updateVersion = function() {
+    var pkg = path.join(__dirname, '../../../package.json');
+
+    require.cache[pkg].exports.version = 'new.version';
+  };
+
+  it('starts with one instance in the process', function() {
+    expect(logShim.countAll()).to.equal(1);
+  });
+
+  it('on delete cache/require, creates second item on version array', function() {
+    deleteShimCache();
+
+    require('../../../src/server');
+    expect(logShim.countAll()).to.equal(2);
+
+    expect(global)
+      .to.have.property(pkg.name)
+      .that.have.property(origVersion);
+    var version = global[pkg.name][origVersion];
+    expect(version).to.have.length(2);
+  });
+
+  it('on delete cache/update version/require, creates second version array', function() {
+    deleteShimCache();
+    updateVersion();
+
+    require('../../../src/server');
+    expect(logShim.countAll()).to.equal(3);
+
+    expect(global)
+      .to.have.property(pkg.name)
+      .that.have.property(origVersion);
+    var version = global[pkg.name][origVersion];
+    expect(version).to.have.length(2);
+
+    expect(global)
+      .to.have.property(pkg.name)
+      .that.have.property('new.version');
+    var newVersion = global[pkg.name][pkg.version];
+    expect(newVersion).to.have.length(1);
+  });
+});
